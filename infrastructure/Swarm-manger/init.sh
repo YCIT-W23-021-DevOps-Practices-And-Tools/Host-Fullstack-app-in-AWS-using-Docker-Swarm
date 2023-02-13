b351eb7ab08107fb514d19d4981f74ea10258e0d
Jason Jafari <me@jasonjafari.com>
2023-02-13 00:31:28 -0500

    Install docker and dependencies

--- infrastructure/Swarm-manger/init.sh
+++ infrastructure/Swarm-manger/init.sh
@@ -18,3 +18,23 @@ sudo hostname -F /etc/hostname
 sudo hostnamectl set-hostname $USE_HOSTNAME
 sudo touch /steps/step003
 
+sudo apt -y install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+touch /steps/step004
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+touch /steps/step005
+
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io
+touch /steps/step006
+
+
+sudo apt install -y git wget curl nano tmux rpl nfs-common
+touch /steps/step007
