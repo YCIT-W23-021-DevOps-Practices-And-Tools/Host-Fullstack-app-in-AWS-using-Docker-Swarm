fdae9eb96988665b7d7cf4b25c98b936f5258802
Jason Jafari <me@jasonjafari.com>
2023-02-12 19:30:40 -0500

    Make sure you are able to ssh swarm-manager

--- infrastructure/Swarm-manger/main-resource.tf
+++ infrastructure/Swarm-manger/main-resource.tf
@@ -5,6 +5,10 @@ resource "aws_instance" "swarm-manager" {
 
     associate_public_ip_address = true
 
+    vpc_security_group_ids = [
+        aws_security_group.swarm-manager-internet-and-ssh.id,
+    ]
+
 
     tags = {
         Name = var.name
@@ -18,4 +22,35 @@ resource "aws_instance" "swarm-manager" {
         create_before_destroy = true
     }
 
+}
+
+
+resource "aws_security_group" "swarm-manager-internet-and-ssh" {
+    name       = "security-group-swarm-servers-internet-and-ssh"
+    tags = {
+        Name     = "security-group-swarm-servers-internet-and-ssh"
+    }
+
+    ingress {
+        from_port       = 0
+        to_port         = 0
+        protocol        = -1
+        self = true
+    }
+
+    ingress {
+        from_port         = 22
+        to_port           = 22
+        protocol          = "tcp"
+        cidr_blocks       = ["0.0.0.0/0"]
+    }
+
+
+
+    egress {
+        from_port       = 0
+        to_port         = 0
+        protocol        = -1
+        cidr_blocks     = ["0.0.0.0/0"]
+    }
 }
