b5d7138d5f88f2e9620207eae1bc2e8e647b1c5b
Jason Jafari <me@jasonjafari.com>
2023-02-20 10:59:45 -0500

    Add sensitive = true for sensitive variables

--- infrastructure/Swarm-manger/variables.tf
+++ infrastructure/Swarm-manger/variables.tf
@@ -1,8 +1,14 @@
 variable "name" {}
 variable "ami" {}
 variable "instance_type" {}
-variable "key_name" {}
+variable "key_name" {
+    sensitive = true
+}
 variable "domain-name" {}
 variable "serverhostname" {}
-variable "DOCKER_LOGIN_ACCESS_TOKEN" {}
-variable "DOCKER_LOGIN_USERNAME" {}
+variable "DOCKER_LOGIN_ACCESS_TOKEN" {
+    sensitive = true
+}
+variable "DOCKER_LOGIN_USERNAME" {
+    sensitive = true
+}
