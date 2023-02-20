fe43b49176b4602fe3bc1de96e638e3b3c0c8ca8
Jason Jafari <me@jasonjafari.com>
2023-02-20 13:04:52 -0500

    OPEN http and Https protocols for swarm manager

--- infrastructure/Swarm-manger/main-resource.tf
+++ infrastructure/Swarm-manger/main-resource.tf
@@ -56,6 +56,20 @@ resource "aws_security_group" "swarm-manager-internet-and-ssh" {
         cidr_blocks       = ["0.0.0.0/0"]
     }
 
+    ingress {
+        from_port         = 80
+        to_port           = 80
+        protocol          = "tcp"
+        cidr_blocks       = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port         = 443
+        to_port           = 443
+        protocol          = "tcp"
+        cidr_blocks       = ["0.0.0.0/0"]
+    }
+
 
 
     egress {
