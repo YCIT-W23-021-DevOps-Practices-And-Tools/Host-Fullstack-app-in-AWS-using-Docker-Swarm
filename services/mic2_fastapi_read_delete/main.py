1cd04a3927e10cf21cbc13520c7bba38cd4850e0
Jason Jafari <me@jasonjafari.com>
2023-03-12 23:26:32 -0400

    Add users endpoint in microservice 2

--- services/mic2_fastapi_read_delete/main.py
+++ services/mic2_fastapi_read_delete/main.py
@@ -1,8 +1,28 @@
 from fastapi import FastAPI
+import os
+import mysql.connector
 
 app = FastAPI()
+
+
+mydb = mysql.connector.connect(
+    host=os.environ.get('DB_DOCKER_HOST'),
+    port=os.environ.get('DB_DOCKER_PORT'),
+    user=os.environ.get('DB_DOCKER_USER'),
+    password=os.environ.get('DB_DOCKER_PASSWORD'),
+    database=os.environ.get('DB_DOCKER_DB_NAME')
+)
+
+mycursor = mydb.cursor(dictionary=True)
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/users")
+async def root():
+    mycursor.execute("SELECT * FROM users")
+    users = mycursor.fetchall()
+    return users
