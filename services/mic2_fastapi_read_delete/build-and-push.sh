afa85bfeaac1fff51ace466691c75cc97fa33659
Jason Jafari <me@jasonjafari.com>
2023-03-13 17:48:50 -0400

    build and production image functionality for ycit021_prod_mic2_fastapi_read_delete

---
+++ services/mic2_fastapi_read_delete/build-and-push.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+pushd $SERVICE_DIR > /dev/null
+    rm -rf venv/
+
+    docker build -t "$DOCKER_LOGIN_USERNAME/ycit021_prod_$SERVICE_NAME:$PROJECT_VERSION" -f DockerfileImagePush .
+
+    docker push "$DOCKER_LOGIN_USERNAME/ycit021_prod_$SERVICE_NAME:$PROJECT_VERSION"
+
+popd > /dev/null
