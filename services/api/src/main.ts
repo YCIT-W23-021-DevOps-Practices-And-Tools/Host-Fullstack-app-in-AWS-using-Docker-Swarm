45120e32a7892de4acff02450abde4a77bc101cf
Jason Jafari <me@jasonjafari.com>
2023-03-13 00:58:20 -0400

    Add basic authentication layer on api

--- services/api/src/main.ts
+++ services/api/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as basicAuth from 'express-basic-auth';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const users = {};
+  users[`${process.env.API_USER_NAME}`] = process.env.API_PASSWORD;
+  app.use(
+    basicAuth({
+      users: users,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
