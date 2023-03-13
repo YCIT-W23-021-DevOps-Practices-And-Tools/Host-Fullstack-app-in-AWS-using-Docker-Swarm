54a36816ba966def493bc3f70d6565707739be91
Jason Jafari <me@jasonjafari.com>
2023-03-13 17:35:59 -0400

    build and production image functionality for front-end

---
+++ services/front-end/build-and-push.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+pushd $SERVICE_DIR > /dev/null
+    rm -rf node_modules/
+
+    rm -rf build/
+
+    docker build -t "$DOCKER_LOGIN_USERNAME/ycit021_prod_$SERVICE_NAME:$PROJECT_VERSION" -f DockerfileImagePush .
+
+    docker push "$DOCKER_LOGIN_USERNAME/ycit021_prod_$SERVICE_NAME:$PROJECT_VERSION"
+
+popd > /dev/null
