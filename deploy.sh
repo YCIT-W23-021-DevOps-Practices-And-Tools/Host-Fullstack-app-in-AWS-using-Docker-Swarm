df6c3c3644e14921a5e364699d5fa450af357a91
Jason Jafari <me@jasonjafari.com>
2023-03-27 17:20:58 -0400

    deploy secrets before deployment

--- deploy.sh
+++ deploy.sh
@@ -14,13 +14,17 @@ if [[ "$1" == "infra" ]]; then
 
     echo "run services/mysql/prod-init.sql in prod db"
     mysql -u $rds_username -p$rds_password --host=$rds_host < services/mysql/prod-init.sql
+    exit 0
 
 fi
 
 
 
 if [[ "$1" == "deploy_secrets" ]]; then
+    ssh-keygen -R "swarm-manager.public.codevteacher.com" || true
+    ssh root@swarm-manager.public.codevteacher.com "ls -la"
     $HOME_FOLDER/dev.sh deploy_secrets
+    exit 0
 fi
 $HOME_FOLDER/dev.sh build-and-push
 $HOME_FOLDER/dev.sh deploy
